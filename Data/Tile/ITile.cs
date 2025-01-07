d38a98441ff5c039a236f33b1795093ad3b1b423
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-07 23:33:22 +0700

    Started project from scratch

--- Data/Tile/ITile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tile;
-/// <summary>
-///  Interface for a distinct tile
-/// </summary>
-public interface ITile {
-    public int x {set; get;}
-    public int y {set; get;}
-    public TileTypes tileType {set; get;}
-}
