af1587a500e213bc0bc3adda49eff9041ef38953
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-09 20:25:27 +0700

    GameBoard rewritten to implement Cell

--- Core/Interfaces/IGameBoard.cs
+++ Core/Interfaces/IGameBoard.cs
@@ -16,27 +16,16 @@ public interface IGameBoard
     int Height { get; }
 
     /// <summary>
-    /// Возвращает или задаёт содержимое ячейки на игровом поле.
+    /// Двумерный массив клеток, представляющих игровое поле.
     /// </summary>
-    /// <param name="x">Координата X (столбец).</param>
-    /// <param name="y">Координата Y (строка).</param>
-    /// <returns>Тип содержимого клетки (пусто, юнит, препятствие).</returns>
-    CellType GetCellType(int x, int y);
-
-    /// <summary>
-    /// Возвращает или задаёт объект, расположенный на клетке.
-    /// </summary>
-    /// <param name="x">Координата X (столбец).</param>
-    /// <param name="y">Координата Y (строка).</param>
-    /// <returns>Юнит или null, если клетка пуста или содержит препятствие.</returns>
-    IUnit? this[int x, int y] { get; set; }
+    ICell[,] Cells { get; }
 
     /// <summary>
     /// Проверяет, является ли ячейка доступной для перемещения.
     /// </summary>
     /// <param name="x">Координата X.</param>
     /// <param name="y">Координата Y.</param>
-    /// <returns>true, если ячейка доступна для перемещения (пустая или с юнитом); иначе false.</returns>
+    /// <returns>true, если ячейка доступна для перемещения; иначе false.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Выбрасывается, если координаты выходят за пределы игрового поля.</exception>
     bool IsCellAvailable(int x, int y);
 }
-
