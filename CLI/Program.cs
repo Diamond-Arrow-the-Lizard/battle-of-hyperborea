ee54614e395707be595b74a5c219fe82d31bb67b
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-09 21:03:23 +0700

    Tested that CLI can interact with models

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace BoH.CLI;
 
+using BoH.Interfaces;
+using BoH.Models;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("goida");
+        GameBoard gameBoard = new(10, 10);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < 10; j++)
+            {
+                var currentCell = gameBoard[i, j];
+                if (currentCell.Content == null)
+                {
+                    Console.Write("O ");
+                }
+                else if (currentCell.Content.GetType() == typeof(IUnit))
+                {
+                    Console.Write("T ");
+                }
+            }
+        }
     }
 }
