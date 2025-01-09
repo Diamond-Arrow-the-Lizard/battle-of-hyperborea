866a3fb90f141f09dd2e5b80a1553a0f4e1e3afd
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-09 21:15:00 +0700

    Added BaseUnit class, changed OwnedId to Team

--- Core/Units/BaseUnit.cs
+++ Core/Units/BaseUnit.cs
@@ -1,9 +1,8 @@
-namespace BoH.Interfaces;
+﻿namespace BoH.Units;
 
-/// <summary>
-/// Представляет юнита в игре с базовыми характеристиками и действиями.
-/// </summary>
-public interface IUnit
+using BoH.Interfaces;
+
+public class BaseUnit : IUnit
 {
     /// <summary>
     /// Кортеж-текущая позиция юнита на игровом поле.
@@ -16,68 +15,77 @@ public interface IUnit
     /// <summary>
     /// Уникальный идентификатор юнита.
     /// </summary>
-    public string UnitId { get; }
+    public string UnitId { get; } = Guid.NewGuid().ToString();
 
     /// <summary>
     /// Название команды, которой принадлежит юнит.
     /// </summary>
-    public string Team { get; }
+    public string Team { get; } = "Dev";
 
     /// <summary>
     /// Иконка, представляющая юнита на игровом поле.
     /// </summary>
-    public char Icon { get; set; }
+    public char Icon { get; set; } = 'T';
 
     /// <summary>
     /// Текущее количество очков здоровья юнита.
     /// </summary>
-    public int Hp { get; set; }
+    public int Hp { get; set; } = 100;
 
     /// <summary>
     /// Скорость передвижения юнита (количество клеток за ход).
     /// </summary>
-    public int Speed { get; set; }
+    public int Speed { get; set; } = 3;
 
     /// <summary>
     /// Урон, наносимый юнитом при атаке.
     /// </summary>
-    public int Damage { get; set; }
+    public int Damage { get; set; } = 20;
 
     /// <summary>
     /// Значение защиты юнита, уменьшающее входящий урон.
     /// </summary>
-    public int Defence { get; set; }
+    public int Defence { get; set; } = 0;
 
     /// <summary>
     /// Тип или класс юнита (например, пехота, танк и т.д.).
     /// </summary>
-    public string UnitType { get; }
+    public string UnitType { get; } = "Melee";
 
     /// <summary>
     /// Показывает, находится ли юнит в состоянии оглушения или не может выполнять действия.
     /// </summary>
-    public bool IsStunned { get; set; }
+    public bool IsStunned { get; set; } = false;
 
     /// <summary>
     /// Коллекция способностей, которыми обладает юнит.
     /// </summary>
-    public List<IAbility> Abilities { get; }
+    public List<IAbility> Abilities { get; } = new();
 
     /// <summary>
     /// Применяет урон к юниту, уменьшая его очки здоровья.
     /// </summary>
     /// <param name="amount">Количество урона для применения.</param>
-    void TakeDamage(int amount);
+    public void TakeDamage(int amount)
+    {
+
+    }
 
     /// <summary>
     /// Перемещает юнита в новую позицию на игровом поле.
     /// </summary>
     /// <param name="newPosition">Новая позиция, в которую нужно переместить юнита.</param>
-    void Move((int X, int Y) newPosition);
+    public void Move((int X, int Y) newPosition)
+    {
+
+    }
 
     /// <summary>
     /// Вычисляет количество урона, который юнит наносит при атаке.
     /// </summary>
     /// <returns>Вычисленный урон.</returns>
-    int CalculateAttackDamage();
+    public int CalculateAttackDamage()
+    {
+        return 0;
+    }
 }
