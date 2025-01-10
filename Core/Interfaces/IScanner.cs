c6844bb260abbafc3ee1ec9292ab66e5afdbab77
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-10 20:58:38 +0700

    Added Scanner that gets cells around one point and returns a list of scanned cells

---
+++ Core/Interfaces/IScanner.cs
@@ -0,0 +1,22 @@
+namespace BoH.Interfaces;
+
+using System.Collections.Generic;
+
+/// <summary>
+/// Интерфейс для объекта, способного сканировать клетки игрового поля вокруг заданной клетки.
+/// </summary>
+public interface IScanner
+{
+    /// <summary>
+    /// Радиус сканирования в клетках.
+    /// </summary>
+    int Range { get; }
+
+    /// <summary>
+    /// Сканирует клетки вокруг заданной клетки на игровом поле в пределах радиуса.
+    /// </summary>
+    /// <param name="scanningCell">Клетка, вокруг которой производится сканирование.</param>
+    /// <param name="gameBoard">Игровое поле, на котором производится сканирование.</param>
+    /// <returns>Коллекция клеток в пределах радиуса вокруг заданной клетки.</returns>
+    IEnumerable<ICell> Scan(ICell scanningCell, IGameBoard gameBoard);
+}
