da24d3e528c9145d56d0fbe2a4f57d04fe75a1c9
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-10 20:14:51 +0700

    Added documentation

--- Core/Interfaces/IGameBoardService.cs
+++ Core/Interfaces/IGameBoardService.cs
@@ -1,18 +1,47 @@
 namespace BoH.Interfaces;
 
-
+/// <summary>
+/// Интерфейс для управления игровым полем.
+/// </summary>
 public interface IGameBoardService
 {
+    /// <summary>
+    /// Генерирует новое игровое поле заданного размера.
+    /// </summary>
+    /// <param name="width">Ширина игрового поля (количество столбцов).</param>
+    /// <param name="length">Длина игрового поля (количество строк).</param>
+    /// <returns>Экземпляр <see cref="IGameBoard"/>, представляющий игровое поле.</returns>
     IGameBoard GenerateGameBoard(int width, int length);
 
-    void AddObjectToGameBoard(Object? obj, ICell cell);
+    /// <summary>
+    /// Добавляет объект в указанную клетку игрового поля.
+    /// </summary>
+    /// <param name="obj">Объект для добавления.</param>
+    /// <param name="cell">Целевая клетка игрового поля.</param>
+    void AddObjectToGameBoard(object? obj, ICell cell);
 
-    void RemoveObjectFromGameBoard(Object? obj, ICell cell);
+    /// <summary>
+    /// Удаляет объект из указанной клетки игрового поля.
+    /// </summary>
+    /// <param name="obj">Объект для удаления.</param>
+    /// <param name="cell">Целевая клетка игрового поля.</param>
+    void RemoveObjectFromGameBoard(object? obj, ICell cell);
 
+    /// <summary>
+    /// Асинхронно сохраняет текущее состояние игрового поля.
+    /// </summary>
+    /// <returns>Задача, представляющая процесс сохранения.</returns>
     Task SaveGameBoardAsync();
 
+    /// <summary>
+    /// Асинхронно загружает состояние игрового поля.
+    /// </summary>
+    /// <returns>Задача, представляющая процесс загрузки.</returns>
     Task LoadGameBoardAsync();
 
+    /// <summary>
+    /// Асинхронно удаляет текущее состояние игрового поля.
+    /// </summary>
+    /// <returns>Задача, представляющая процесс удаления.</returns>
     Task DeleteGameBoardAsync();
-
 }
