060d8c82def7f101cfd32635e0480f14c3656b5b
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-10 20:13:42 +0700

    Added GameBoardService, WIP

--- Core/Services/GameBoardService.cs
+++ Core/Services/GameBoardService.cs
@@ -1,6 +1,45 @@
-﻿namespace Services;
+﻿namespace BoH.Services;
 
-public class Class1
+using BoH.Interfaces;
+using BoH.Models;
+using System.Threading.Tasks;
+
+public class GameBoardService : IGameBoardService
 {
+    public IGameBoard GenerateGameBoard(int width, int length)
+    {
+        // TODO случайная герерация препятствий
+        var gameBoard = new GameBoard(width, length);
+        return gameBoard;
+    }
 
+    public void AddObjectToGameBoard(Object? obj, ICell cell)
+    {
+        if(obj == null) return;
+        cell.Content = obj;
+    }
+
+    public void RemoveObjectFromGameBoard(Object? obj, ICell cell)
+    {
+        if(obj == null) return;
+        cell.Content = null;
+    }
+
+    public async Task SaveGameBoardAsync()
+    {
+        // TODO
+        await Task.Delay(0);
+    }
+
+    public async Task LoadGameBoardAsync()
+    {
+        // TODO
+        await Task.Delay(0);
+    }
+
+    public async Task DeleteGameBoardAsync()
+    {
+        // TODO
+        await Task.Delay(0);
+    }
 }
