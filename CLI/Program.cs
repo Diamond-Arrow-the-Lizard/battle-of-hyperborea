55c81400e895950741bce5e47bdb32210d34c62f
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-10 19:26:06 +0700

    Added static class for rendering the game board

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -9,23 +9,6 @@ public class Program
     public static void Main(string[] args)
     {
         GameBoard gameBoard = new(10, 10);
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine();
-            for (int j = 0; j < 10; j++)
-            {
-                var currentCell = gameBoard[i, j];
-                IUnit unit = new BaseUnit();
-                currentCell.Content = unit;
-                if (currentCell.Content == null)
-                {
-                    Console.Write("O ");
-                }
-                else if (currentCell.Content.GetType() == typeof(BaseUnit))
-                {
-                    Console.Write($"{unit.Icon} ");
-                }
-            }
-        }
+        GameBoardRenderer.DrawBoard(gameBoard);
     }
 }
