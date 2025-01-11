59ce4655fbb07d6d33b96b10ee735141e90c1863
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-11 22:51:36 +0700

    Added SelfCare passive ability

---
+++ Core/Abilities/SelfCare.cs
@@ -0,0 +1,40 @@
+namespace BoH.Abilities;
+
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using BoH.Interfaces;
+public class SelfCare : IAbility
+{
+    /// <inheritdoc/> 
+    public string AbilityId { get; }
+
+    /// <inheritdoc/> 
+    public string Name { get; } = "Самолечение";
+
+    /// <inheritdoc/> 
+    public string Description { get; } = "В конце хода юнит лечит себя на 1-6 здоровья";
+
+    /// <inheritdoc/> 
+    public bool IsActive { get; } = false;
+
+    /// <inheritdoc/> 
+    public bool Activate(IUnit user, IUnit? target = null)
+    {
+        Random rnd = new Random();
+        target = user;
+        user.Heal(rnd.Next(1, 7));
+        return true;
+    }
+
+    /// <inheritdoc/> 
+    public void Update()
+    {
+
+    }
+
+    public SelfCare()
+    {
+        AbilityId = Guid.NewGuid().ToString();
+    }
+
+}
