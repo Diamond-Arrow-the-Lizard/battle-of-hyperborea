e2d259f10da3e548ad244a24404df786fcec44ae
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-11 23:29:12 +0700

    Fix: _maxHealth field is now a MaxHealth virtual property to adress the fact that Heal method always used _maxHealth value of BaseUnit

--- Core/Units/RusUnits/RusArcher.cs
+++ Core/Units/RusUnits/RusArcher.cs
@@ -5,14 +5,13 @@ using BoH.Interfaces;
 
 public class RusArcher : BaseUnit
 {
-    private int _maxHealth = 10;
-
-    public int AttackRange {get; set;} = 5;
+    protected override int MaxHealth { get; } = 10;
+    public int AttackRange { get; set; } = 5;
 
     public RusArcher() : base('Я', "Rus", UnitType.Range)
     {
         Abilities.Add(new SelfCare());
-        Hp = _maxHealth;
+        Hp = MaxHealth;
         Defence = 2;
         DamageDices = 3;
     }
--- Core/Units/RusUnits/RusWarrior.cs
+++ Core/Units/RusUnits/RusWarrior.cs
@@ -4,12 +4,11 @@ using BoH.Abilities;
 
 public class RusWarrior : BaseUnit
 {
-    private int _maxHealth = 20;
-
+    protected override int MaxHealth { get; } = 10;
     public RusWarrior() : base('R', "Rus")
     {
         Abilities.Add(new StunningBlow());
-        Hp = _maxHealth;
+        Hp = MaxHealth;
         Defence = 8;
         DamageDices = 2;
     }
