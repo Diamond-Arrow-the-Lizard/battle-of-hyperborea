c370818b606d98c98604edbb818e082e04bfbb64
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-12 00:12:17 +0700

    Redesigned GameBoardRenderer for eye-candy

--- CLI/GameBoardRenderer.cs
+++ CLI/GameBoardRenderer.cs
@@ -7,23 +7,57 @@ public static class GameBoardRenderer
 {
     public static void DrawBoard(GameBoard board)
     {
+        // Верхняя граница
+        Console.Write("╔");
         for (int x = 0; x < board.Width; x++)
         {
-            Console.WriteLine();
-            for (int y = 0; y < board.Width; y++)
+            Console.Write("═");
+            if (x < board.Width - 1)
+                Console.Write("╦");
+        }
+        Console.WriteLine("╗");
+
+        // Рисуем строки игрового поля
+        for (int y = 0; y < board.Height; y++)
+        {
+            Console.Write("║");
+            for (int x = 0; x < board.Width; x++)
             {
                 var cellContent = board[x, y].Content;
 
                 char symbol = cellContent switch
                 {
-                    null => '0',
+                    null => ' ', // Пустая клетка
                     Obstacle obstacle => obstacle.Icon,
                     IUnit unit => unit.Icon,
                     _ => '?'
                 };
-                Console.Write($"{symbol} ");
-                
+                Console.Write($"{symbol}║");
+            }
+            Console.WriteLine();
+
+            // Горизонтальная разделительная линия (кроме последней строки)
+            if (y < board.Height - 1)
+            {
+                Console.Write("╠");
+                for (int x = 0; x < board.Width; x++)
+                {
+                    Console.Write("═");
+                    if (x < board.Width - 1)
+                        Console.Write("╬");
+                }
+                Console.WriteLine("╣");
             }
         }
+
+        // Нижняя граница
+        Console.Write("╚");
+        for (int x = 0; x < board.Width; x++)
+        {
+            Console.Write("═");
+            if (x < board.Width - 1)
+                Console.Write("╩");
+        }
+        Console.WriteLine("╝");
     }
 }
