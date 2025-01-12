cbd5d7a7142fa704ca24c33d2351d4e2c60fd649
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-13 02:38:45 +0700

    Implemented unit moving

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -68,12 +68,65 @@ public class Program
         {
             teamUnits[teamCells[i].Position] = teamUnitCells[i];
         }
-        foreach (var unit in teamUnits)
-        {
-            Console.WriteLine($"{unit.Value.UnitName} ({unit.Key.x};{unit.Key.y})");
-        }
 
-        
+        while (true)
+        {
+            int availableUnits = 0;
+            Console.WriteLine("Available Units:");
+            foreach (var unit in teamUnits)
+            {
+                if (unit.Value.MadeTurn == true)
+                {
+                    ++availableUnits;
+                    Console.WriteLine($"{unit.Value.UnitName} ({unit.Key.x};{unit.Key.y})");
+                }
+            }
+            if (availableUnits == 0)
+            {
+                Console.WriteLine("No Units Available");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Chosen Unit: ");
+                Console.Write("X = ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                (int x, int y) userInput = (x, y);
+                if (teamUnits.ContainsKey(userInput))
+                {
+                    var ChosenUnit = teamUnits[userInput];
+                    Console.WriteLine($"Chosen unit: {ChosenUnit.UnitName}");
+                    Scanner scanner = new Scanner(ChosenUnit.Speed);
+                    IEnumerable<ICell> availableMoves = scanner.Scan(board[userInput.x, userInput.y], board);
+                    GameBoardRenderer.DrawBoard(board, board[userInput.x, userInput.y], ChosenUnit.Speed);
+                    Console.WriteLine($"Available moves: ");
+                    foreach (var i in availableMoves) Console.Write($"({i.Position.X};{i.Position.Y}) | ");
+                    Console.WriteLine("\nMove unit to...");
+
+                    Console.Write("X = ");
+                    int newX = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Y = ");
+                    int newY = Convert.ToInt32(Console.ReadLine());
+                    Cell move = new((newX, newY));
+                    bool isLegal = availableMoves.Any(c => c.Position == move.Position);
+                    if(newX >= 0 && newY >= 0 && newX < board.Width && newY < board.Height && board[newX, newY].Content == null && isLegal)
+                    {
+                        teamUnits[(newX, newY)] = ChosenUnit;
+                        board[x, y].Content = null;
+                        board[newX, newY].Content = ChosenUnit;
+                        teamUnits.Remove(userInput);
+                        GameBoardRenderer.DrawBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal move");
+                    }
+                }
+            }
+
+        }
 
     }
 }
