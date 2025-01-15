20d3d6c1c4aa71c5ee2461c29597ecaf1b31d354
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-16 02:03:11 +0700

    Added GUI for Save-Load buttons

---
+++ GUI/Views/LoadButtonView.axaml.cs
@@ -0,0 +1,18 @@
+namespace BoH.GUI.Views
+{
+    using Avalonia.Controls;
+
+    /// <summary>
+    /// Представляет View для кнопки загрузки игрового поля.
+    /// </summary>
+    public partial class LoadButtonView : UserControl
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="LoadButtonView"/>.
+        /// </summary>
+        public LoadButtonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
