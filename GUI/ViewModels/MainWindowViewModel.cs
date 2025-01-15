1d5e4104d36fbea88a7d598a6c41aeaa059466a6
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-01-16 05:11:04 +0700

    Deleted Views to adress problems, still fixing

--- GUI/ViewModels/MainWindowViewModel.cs
+++ GUI/ViewModels/MainWindowViewModel.cs
@@ -4,37 +4,49 @@
     using BoH.Services;
     using BoH.Models;
     using System.ComponentModel;
+    using System;
+    using System.Collections.Generic;
+    using BoH.Units;
 
     /// <summary>
     /// Основная ViewModel для главного окна, которая включает все необходимые ViewModel-и для кнопок и игрового поля.
     /// </summary>
     public class MainWindowViewModel 
     {
-        private GameBoard gameBoard = new GameBoard(5, 5);
+        
+        private readonly IGameBoardService _gameBoardService;
+
+        private GameBoard gameBoard = new GameBoard(1, 1);
         /// <summary>
         /// ViewModel для игрового поля.
         /// </summary>
         public GameBoardViewModel GameBoardViewModel { get; }
 
         /// <summary>
-        /// ViewModel для кнопки сохранения.
-        /// </summary>
-        public SaveButtonViewModel SaveButtonViewModel { get; }
-
-        /// <summary>
-        /// ViewModel для кнопки загрузки.
-        /// </summary>
-        public LoadButtonViewModel LoadButtonViewModel { get; }
-
-        /// <summary>
         /// Конструктор для создания экземпляра <see cref="MainWindowViewModel"/>.
         /// </summary>
         /// <param name="gameBoardService">Сервис для работы с игровым полем.</param>
-        public MainWindowViewModel(GameBoardService gameBoardService)
+        public MainWindowViewModel(IGameBoardService gameBoardService)
         {
-            GameBoardViewModel = new GameBoardViewModel(gameBoardService, gameBoard);
-            SaveButtonViewModel = new SaveButtonViewModel(gameBoardService);
-            LoadButtonViewModel = new LoadButtonViewModel(gameBoardService);
+
+            Dictionary<string, List<IUnit>> teams = new();
+                teams["Rus"] = new List<IUnit>
+            {
+                new RusArcher(), new RusArcher(),
+                new RusWarrior(), new RusWarrior(),
+                new RusWarrior(), new RusWarrior()
+            };
+                teams["Lizard"] = new List<IUnit>
+            {
+                new LizardArcher(), new LizardArcher(),
+                new LizardWarrior(), new LizardWarrior(),
+                new LizardWarrior(), new LizardWarrior()
+            };
+
+            _gameBoardService = gameBoardService ?? throw new ArgumentNullException(nameof(gameBoardService));;
+            GameBoardViewModel = new GameBoardViewModel(gameBoard);
         }
+        
     }
+    
 }
