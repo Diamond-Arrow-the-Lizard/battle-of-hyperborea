201412d972705479f06c13b75e7634ef6cd62f97
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-02-03 05:02:54 +0700

    Refactored GenerateGameBoard() method in GameBoardService

--- Core/Services/GameBoardService.cs
+++ Core/Services/GameBoardService.cs
@@ -10,58 +10,90 @@ using System.Threading.Tasks;
 public class GameBoardService : IGameBoardService
 {
     /// <inheritdoc/>
-    /// <exception cref="OutOfMemoryException"/> 
-    // Юниты появляются в диаметрально противоположных углах
-    public IGameBoard GenerateGameBoard(int width, int length, Dictionary<string, List<IUnit>> teams)
+    public IGameBoard GenerateGameBoard(int width, int height, IEnumerable<IUnit> units, ref IPlayer[] players)
     {
-        var gameBoard = new GameBoard(width, length);
-        return gameBoard;
-        /*
+        // Проверка входных данных
+        if (players == null || players.Length < 2)
+            throw new ArgumentException("Массив игроков должен содержать минимум двух игроков.", nameof(players));
+
         Random rnd = new Random();
-        var gameBoard = new GameBoard(width, length);
-        List<string> teamNames = teams.Keys.ToList();
-        List<IIconHolder> teamOne = teams[teamNames[0]];
-        List<IUnit> teamTwo = teams[teamNames[1]];
+        var gameBoard = new GameBoard(width, height);
 
-        if (teamOne.Count > gameBoard.Width - 1 || teamTwo.Count > gameBoard.Width - 1)
-            throw new OutOfMemoryException("Кол-во юнитов превышает длину поля");
+        // Сбор имен команд из всех юнитов
+        List<string> teamNames = new List<string>();
+        foreach (var unit in units)
+            teamNames.Add(unit.Team); 
 
-        int x = 0;
-        foreach (var unit in teamOne)
+        var uniqueTeams = teamNames.Distinct().ToList();
+        if (uniqueTeams.Count != 2)
+            throw new InvalidOperationException("Число команд не равно двум.");
+
+        // Распределение юнитов по игрокам в зависимости от команды
+        foreach (var unit in units)
         {
-            gameBoard[x, width - 1].Content = unit;
-            ++x;
+            if (unit.Team == uniqueTeams[0])
+                players[0].AddUnit(unit);
+            else if (unit.Team == uniqueTeams[1])
+                players[1].AddUnit(unit);
+            else
+                throw new InvalidOperationException($"Неизвестная команда: {unit.Team}");
         }
 
-        x = length - 1;
-        foreach (var unit in teamTwo)
+        // Проверка: число юнитов для каждого игрока не должно превышать допустимое число (например, не больше количества столбцов)
+        if (players[0].Units.Count > gameBoard.Width || players[1].Units.Count > gameBoard.Width)
+            throw new InvalidOperationException("Количество юнитов превышает допустимое число для игрового поля.");
+
+        // Размещение юнитов в диаметрально противоположных углах:
+        // Для наглядности, выберем следующую стратегию:
+        // - Юниты первого игрока (команда uniqueTeams[0]) размещаются по верхней строке слева направо.
+        // - Юниты второго игрока (команда uniqueTeams[1]) – по нижней строке справа налево.
+        int pos = 0;
+        foreach (var unit in players[0].Units)
         {
-            gameBoard[x, 0].Content = unit;
-            --x;
+            if (unit is IIconHolder iconHolder)
+            {
+                // Верхняя строка (индекс 0), колонки слева направо
+                gameBoard[pos, 0].Content = iconHolder;
+                pos++;
+            }
         }
 
+        pos = 0;
+        foreach (var unit in players[1].Units)
+        {
+            if (unit is IIconHolder iconHolder)
+            {
+                // Нижняя строка (индекс Height - 1), колонки справа налево
+                gameBoard[gameBoard.Width - 1 - pos, gameBoard.Height - 1].Content = iconHolder;
+                pos++;
+            }
+        }
+
+        // Размещение препятствий. Количество препятствий случайное от 0 до gameBoard.Width - 1.
         int obstacleCount = rnd.Next(0, gameBoard.Width);
         int failedPlacements = 0;
-        while (obstacleCount != 0)
+        while (obstacleCount > 0)
         {
-            x = rnd.Next(0, gameBoard.Width);
+            int x = rnd.Next(0, gameBoard.Width);
             int y = rnd.Next(0, gameBoard.Height);
             if (gameBoard[x, y].Content != null)
             {
-                ++failedPlacements;
+                failedPlacements++;
                 if (failedPlacements == 3)
                     break;
             }
             else
             {
                 gameBoard[x, y].Content = new Obstacle();
-                --obstacleCount;
+                obstacleCount--;
+                // Сброс счётчика неудачных попыток после успешного размещения
+                failedPlacements = 0;
             }
         }
 
         return gameBoard;
-        */
     }
+
 
     /// <inheritdoc/>
     public void AddObjectToGameBoard(IIconHolder? obj, ICell cell)
