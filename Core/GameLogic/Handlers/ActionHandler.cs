e3b117fdae90056b538e4ee50faaed9a82e8a3ed
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2025-02-05 00:51:13 +0700

    Finished: ProcessScanner() method in TurnManager Refactored: adjustments in TurnManager for changes in IUnit interface Linked interfaces to their implementations Added documentation for GameLogic-related classes && interfaces

--- Core/GameLogic/Handlers/ActionHandler.cs
+++ Core/GameLogic/Handlers/ActionHandler.cs
@@ -2,38 +2,50 @@ namespace BoH.GameLogic;
 
 using BoH.Interfaces;
 
+/// <summary>
+/// Реализация обработчика действий юнитов.
+/// </summary>
 public class ActionHandler : IActionHandler
 {
     private IGameBoard _gameBoard;
+    
+    /// <inheritdoc/>
     public event Action<IGameBoard>? OnUpdatingGameBoard;
 
+    /// <summary>
+    /// Создает экземпляр обработчика действий.
+    /// </summary>
+    /// <param name="gameBoard">Игровое поле.</param>
     public ActionHandler(IGameBoard gameBoard)
     {
         _gameBoard = gameBoard;
     }
+    
+    /// <inheritdoc/>
     public void HandleMovement(IUnit movingUnit, ICell destination, List<ICell> legalMoves)
     {
-        if(movingUnit.OccupiedCell == null) throw new ArgumentNullException("Юнит не был поставлен на поле.");
-        if(!legalMoves.Contains(destination)) throw new InvalidOperationException("Клетка не находится в радиусе передвижения.");
-        if(destination.IsOccupied()) throw new InvalidOperationException("Клетка занята, передвижение невозможно.");
-
+        if (movingUnit.OccupiedCell == null) throw new ArgumentNullException("Юнит не был поставлен на поле.");
+        if (!legalMoves.Contains(destination)) throw new InvalidOperationException("Клетка не находится в радиусе передвижения.");
+        if (destination.IsOccupied()) throw new InvalidOperationException("Клетка занята, передвижение невозможно.");
+        
         (int x, int y) originalPosition = movingUnit.OccupiedCell.Position;
-
         _gameBoard[originalPosition.x, originalPosition.y].Content = null;
-
+        
         movingUnit.OccupiedCell = destination;
         destination.Content = movingUnit as IIconHolder;
-
+        
         movingUnit.ChangeTurnPhase();
         OnUpdatingGameBoard?.Invoke(_gameBoard);
     }
+    
+    /// <inheritdoc/>
     public void HandleAttack(IUnit attacker, ICell targetedCell, List<ICell> legalAttackLocations)
     {
-        if(!legalAttackLocations.Contains(targetedCell)) throw new InvalidOperationException("Клетка не находится в радиусе атаки.");
-        if(targetedCell.Content is IObstacle) throw new InvalidOperationException("Нельзя атаковать препятствие.");
-        if(targetedCell.Content is null) throw new InvalidOperationException("Выбрана пустая клетка.");
+        if (!legalAttackLocations.Contains(targetedCell)) throw new InvalidOperationException("Клетка не находится в радиусе атаки.");
+        if (targetedCell.Content is IObstacle) throw new InvalidOperationException("Нельзя атаковать препятствие.");
+        if (targetedCell.Content is null) throw new InvalidOperationException("Выбрана пустая клетка.");
 
-        if(targetedCell.Content is IUnit target)
+        if (targetedCell.Content is IUnit target)
         {
             attacker.Attack(target);
             attacker.ChangeTurnPhase();
@@ -41,25 +53,27 @@ public class ActionHandler : IActionHandler
         }
         else throw new InvalidDataException("Неизвестный тип объекта в клетке.");
     }
+    
+    /// <inheritdoc/>
     public void HandleAbility(IUnit attacker, IAbility usedAbility, ICell targetedCell, List<ICell> legalAttackLocations)
     {
-        if(!legalAttackLocations.Contains(targetedCell)) throw new InvalidOperationException("Клетка не находится в радиусе атаки.");
-        if(!attacker.Abilities.Contains(usedAbility)) throw new InvalidOperationException("Способность отсутствует у юнита.");
-        if(targetedCell.Content is IObstacle) throw new InvalidOperationException("Нельзя атаковать препятствие.");
-        if(targetedCell.Content is null) throw new InvalidOperationException("Выбрана пустая клетка.");
+        if (!legalAttackLocations.Contains(targetedCell)) throw new InvalidOperationException("Клетка не находится в радиусе атаки.");
+        if (!attacker.Abilities.Contains(usedAbility)) throw new InvalidOperationException("Способность отсутствует у юнита.");
+        if (targetedCell.Content is IObstacle) throw new InvalidOperationException("Нельзя атаковать препятствие.");
+        if (targetedCell.Content is null) throw new InvalidOperationException("Выбрана пустая клетка.");
 
-        if(targetedCell.Content is IUnit target)
+        if (targetedCell.Content is IUnit target)
         {
             usedAbility.Activate(attacker, target);
             attacker.ChangeTurnPhase();
             OnUpdatingGameBoard?.Invoke(_gameBoard);
         }
         else throw new InvalidDataException("Неизвестный тип объекта в клетке.");
-
     }
 
+    /// <inheritdoc/>
     public void HandleSkip(IUnit unit)
     {
-        if(unit.CurrentTurnPhase != TurnPhase.End) unit.ChangeTurnPhase();
+        if (unit.CurrentTurnPhase != TurnPhase.End) unit.ChangeTurnPhase();
     }
 }
--- Core/GameLogic/Handlers/ScannerHandler.cs
+++ Core/GameLogic/Handlers/ScannerHandler.cs
@@ -2,24 +2,32 @@ namespace BoH.GameLogic;
 
 using BoH.Interfaces;
 
+/// <summary>
+/// Реализация обработчика сканирования игрового поля.
+/// </summary>
 public class ScannerHandler : IScannerHandler
 {
-    private IScanner _scanner;
     private IGameBoard _gameBoard;
+    
+    /// <inheritdoc/>
+    public event Action<IGameBoard, List<ICell>>? OnGameBoardScanned;
 
-    public event Action<IGameBoard, IScanner>? OnGameBoardScanned;
-
-    public ScannerHandler(IScanner scanner, IGameBoard gameBoard)
+    /// <summary>
+    /// Создает экземпляр обработчика сканирования.
+    /// </summary>
+    /// <param name="gameBoard">Игровое поле.</param>
+    public ScannerHandler(IGameBoard gameBoard)
     {
-        _scanner = scanner;
         _gameBoard = gameBoard;
     }
 
-    public List<ICell> HandleScan(ICell scanningCell)
+    /// <inheritdoc/>
+    public List<ICell> HandleScan(ICell scanningCell, int range)
     {
+        Scanner scanner = new(range);
         List<ICell> scannedCells = new();
-        scannedCells = (List<ICell>)_scanner.Scan(scanningCell, _gameBoard);
-        OnGameBoardScanned?.Invoke(_gameBoard, _scanner);
+        scannedCells = (List<ICell>)scanner.Scan(scanningCell, _gameBoard);
+        OnGameBoardScanned?.Invoke(_gameBoard, scannedCells);
         return scannedCells;
     }
 }
