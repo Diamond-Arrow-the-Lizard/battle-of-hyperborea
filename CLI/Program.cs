520520d24a85e7d3b14fa3f390b1453ddcfa373f
Diamond_Arrow <diamond1arrow@gmail.com>
2025-02-07 20:50:38 +0700

    Fixed: exceptions && variables in HandleAbility() being too strict TODO add notifications regarding ability failing to be used by a unit

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -36,6 +36,16 @@ public class Program
             new LizardWarrior()
         };
 
+        ConsoleAbilityNotifications abilityNotifications = new ConsoleAbilityNotifications();
+        ConsoleUnitNotifications unitNotifications = new ConsoleUnitNotifications();
+
+        foreach (var i in units)
+        {
+            i.OnAttack += unitNotifications.Notify_UnitAttacked;
+            i.Abilities[0].OnAbilityUsed += abilityNotifications.Notify_AbilityUsed;
+            i.OnTakingDamage += unitNotifications.Notify_UnitRecievedDamage;
+        }
+
         players[0] = new Player("Rus");
         players[1] = new Player("Lizard");
 
@@ -48,20 +58,22 @@ public class Program
         actionHandler.OnUpdatingGameBoard += renderer.Render;
         scannerHandler.OnGameBoardScanned += renderer.ScanRender;
 
-        ConsoleAbilityNotifications abilityNotifications = new ConsoleAbilityNotifications();
-        ConsoleUnitNotifications unitNotifications = new ConsoleUnitNotifications();
 
         TurnManager turnManager = new TurnManager(gameBoard, players, actionHandler, scannerHandler);
 
         turnManager.OnUnitSelected += unitNotifications.Notify_UnitSelected;
+
+
         // ------------------------------------------------------------------------------------------------------
 
         // Ход игрока 1
         turnManager.StartNewRound(players[0]);
-        turnManager.SelectUnit(gameBoard[0, 0]);
+        renderer.Render(gameBoard);
+        turnManager.SelectUnit(gameBoard[1, 0]);
         scannedCells = turnManager.ProcessScanner(ActionType.Move);
         turnManager.ProcessPlayerAction(ActionType.Move, scannedCells, gameBoard[1, 1]);
         turnManager.SelectUnit(gameBoard[1, 1]);
+        scannedCells = turnManager.ProcessScanner(ActionType.Attack);
         turnManager.ProcessPlayerAction(ActionType.Skip);
 
         // ... Проходит ход...
@@ -70,7 +82,32 @@ public class Program
         turnManager.EndTurn();
         gameController.CheckVictoryCondition(players);
         turnManager.StartNewRound(players[1]);
-        turnManager.SelectUnit(gameBoard[7, 7]);
+        turnManager.SelectUnit(gameBoard[6, 7]);
+        scannedCells = turnManager.ProcessScanner(ActionType.Move);
+        turnManager.ProcessPlayerAction(ActionType.Move, scannedCells, gameBoard[4, 4]);
+
+        turnManager.SelectUnit(gameBoard[4, 4]);
+        LizardWarrior selectedUnit = gameBoard[4, 4].Content as LizardWarrior ?? throw new ArgumentNullException();
+        selectedUnit.Abilities[0].OnCooldown += abilityNotifications.Notify_AbilityOnCooldown;
+        
+        scannedCells = turnManager.ProcessScanner(ActionType.Ability);
+        turnManager.ProcessPlayerAction(ActionType.Ability, scannedCells, null, selectedUnit.Abilities[0]);
+
+        Console.WriteLine(selectedUnit.CurrentTurnPhase);
+        Console.WriteLine(selectedUnit.Abilities[0]);
+
+        turnManager.SelectUnit(selectedUnit.OccupiedCell!);
+        scannedCells = turnManager.ProcessScanner(ActionType.Move);
+        Console.WriteLine(selectedUnit.CurrentTurnPhase);
+        Console.WriteLine(selectedUnit.Abilities[0]);
+        turnManager.ProcessPlayerAction(ActionType.Move, scannedCells, gameBoard[2, 1]);
+
+        Console.WriteLine(selectedUnit.CurrentTurnPhase);
+        Console.WriteLine(selectedUnit.Abilities[0]);
+        
+        turnManager.SelectUnit(gameBoard[2, 1]!);
+        scannedCells = turnManager.ProcessScanner(ActionType.Attack);
+        turnManager.ProcessPlayerAction(ActionType.Attack, scannedCells, gameBoard[1, 1]);
 
         // ... Проходит ход...
 
