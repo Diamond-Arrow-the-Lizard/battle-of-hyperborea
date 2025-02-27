3c6096b852f9a4b41ee019290185c003f3f58c8f
Diamond_Arrow <diamond1arrow@gmail.com>
2025-02-27 13:54:35 +0700

    Added: Attack active ability Fixed: Attack turn phase being done in AcitonHandler instead of attacker unit's Attack() method

---
+++ Core/Models/Abilities/ActiveAbilities/Attack.cs
@@ -0,0 +1,58 @@
+using BoH.Interfaces;
+
+namespace BoH.Models;
+
+public class Attack : IAbility
+{
+    /// <inheritdoc/> 
+    public string AbilityId { get; }
+
+    /// <inheritdoc/> 
+    public string Name { get; } = "Атака";
+
+    /// <inheritdoc/> 
+    public string Description { get; } = "Атакуйте врага. Кол-во урона зависит от числа бросаемых кубиков";
+
+    /// <inheritdoc/> 
+    public bool IsActive { get; } = true;
+
+    /// <inheritdoc/> 
+    public int Coolown { set; get; } = 0;
+
+    /// <inheritdoc/> 
+    public event Action<IAbility>? OnAbilityUsed;
+
+    /// <inheritdoc/> 
+    public event Action<IAbility>? OnCooldown = null;
+
+    /// <inheritdoc/> 
+    public bool Activate(IUnit user, IUnit? target = null)
+    {
+        if (target == null) return false;
+
+        else if (Coolown > 0)
+        {
+            OnCooldown?.Invoke(this);
+            return false;
+        }
+
+        else
+        {
+            user.Attack(target);
+            OnAbilityUsed?.Invoke(this);
+            return true;
+        }
+    }
+
+    /// <inheritdoc/> 
+    public void Update()
+    {
+        throw new InvalidOperationException("Обычная атака не имеет кулдауна.");
+    }
+
+    public Attack()
+    {
+        AbilityId = Guid.NewGuid().ToString();
+        OnCooldown = null;
+    }
+}
