f6f92d50deb270337ba8a3995e1f5d5119f7e3d3
Diamond_Arrow <diamond1arrow@gmail.com>
2025-02-28 18:19:36 +0700

    Removed: commented out code Changed: Game board size, it is now bigger

--- Core/GameLogic/Handlers/ActionHandler.cs
+++ Core/GameLogic/Handlers/ActionHandler.cs
@@ -43,24 +43,7 @@ public class ActionHandler : IActionHandler
         movingUnit.ChangeTurnPhase();
         OnUpdatingGameBoard?.Invoke(_gameBoard);
     }
-
-    /*
-    /// <inheritdoc/>
-    public void HandleAttack(IUnit attacker, ICell targetedCell, List<ICell> legalAttackLocations)
-    {
-        if (!legalAttackLocations.Contains(targetedCell)) throw new InvalidOperationException("Клетка не находится в радиусе атаки.");
-        if (targetedCell.Content is IObstacle) throw new InvalidOperationException("Нельзя атаковать препятствие.");
-        if (targetedCell.Content is null) throw new InvalidOperationException("Выбрана пустая клетка.");
-
-        if (targetedCell.Content is IUnit target)
-        {
-            attacker.Attack(target);
-            OnUpdatingGameBoard?.Invoke(_gameBoard);
-        }
-        else throw new InvalidDataException("Неизвестный тип объекта в клетке.");
-    }
-    */
-
+    
     /// <inheritdoc/>
     public void HandleAction(IUnit attacker, IAbility usedAbility, List<ICell> legalAttackLocations, ICell? targetedCell = null)
     {
