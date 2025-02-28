b3a772348e5a4f091c1f77021cf430676fdb110a
Diamond_Arrow <diamond1arrow@gmail.com>
2025-03-01 05:47:22 +0700

    Hopefully merged a release

--- Core/Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Core/Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01728888b0b3e09832bfae32868b0afe9e7ae5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8182f02a8b7230526f3bf775565badb28d7600f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
