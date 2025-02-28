872ce4cfc150643f95e59b8a9ad3ded77036894a
Diamond_Arrow <diamond1arrow@gmail.com>
2025-03-01 04:01:09 +0700

    Added: field for unit description Changed: formatting of active abilities' list

---
+++ GUI/Views/SelectedUnitDetailsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BoH.GUI.Views;
+
+public partial class SelectedUnitDetailsView : UserControl
+{
+    public SelectedUnitDetailsView()
+    {
+        InitializeComponent();
+    }
+}
